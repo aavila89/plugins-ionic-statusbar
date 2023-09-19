2251f25cfc31f7860df26aeefb0ee9a39e158ffc
aavila89 <90415322+aavila89@users.noreply.github.com>
2023-09-19 16:05:19 -0400

    fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.statusbar.cap',
   appName: 'base',
   webDir: 'www',
   server: {
